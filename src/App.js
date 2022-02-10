ca23a564fac36d37f6805a5a8df1e271e67cbdda
monilbid <bid.monil@gmail.com>
2022-02-11 02:08:02 +0300

    Fixed bugs.

--- src/App.js
+++ src/App.js
@@ -2,60 +2,107 @@ import './App.css';
 import { useState } from 'react';
 
 function App() {
-  const [heightUnit, setHeightUnit] = useState("cm");
-  const [weightUnit, setWeightUnit] = useState("kg");
-  const [height, setHeight] = useState();
-  const [weight, setWeight] = useState();
+  const [heightUnit, setHeightUnit] = useState();
+  const [weightUnit, setWeightUnit] = useState();
+  const [isBmiValid, setIsBmiValid] = useState(false);
+  const [bmi, setBmi] = useState();
+  const [bmiCategory, setBmiCategory] = useState();
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    
+    const heightValue = document.getElementById("height").value;
+    const weightValue = document.getElementById("weight").value;
+
+    if (isNaN(heightValue) || isNaN(weightValue) || !heightUnit || !weightUnit) {
+      console.log("Error");
+      setIsBmiValid(false);
+      setBmiCategory();
+      return;
+    }
+
+    const heightMultiplier = heightUnit === "cm" ? 1 : 2.54;
+    const weightDivisor = weightUnit === "kg" ? 1 : 2.04623;
+
+    const height = (heightValue * heightMultiplier) / 100;
+    const weight = weightValue / weightDivisor;
+
+    const calculatedBmi = weight / (height * height);
+
+    setBmi(Math.floor(calculatedBmi * 100) / 100);
+
+    if (calculatedBmi < 18.5) {
+      setBmiCategory("underweight");
+    } else if (calculatedBmi >= 18.5 && calculatedBmi < 25) {
+      setBmiCategory("normal weight");
+    } else if (calculatedBmi >= 25 && calculatedBmi < 30) {
+      setBmiCategory("overweight");
+    } else {
+      setBmiCategory("obese");
+    }
+
+    setIsBmiValid(true);
+  }
+
+  const bmiTextGenerator = () => {
+    let color = "black";
+
+    if (bmiCategory === "underweight") {
+      color = "#ea4c46";
+    } else if (bmiCategory === "normal weight") {
+      color = "green";
+    } else if (bmiCategory === "overweight") {
+      color = "#ea4c46";
+    } else {
+      color = "#dc1c13";
+    }
+
+    return (
+      <>
+        <h3>Your BMI is {bmi}</h3>
+        <h4 style={{color: color}}>You are {bmiCategory}.</h4>
+      </>
+    );
+  }
 
   return (
     <div className="App">
       <h1>BMI Calculator</h1>
       <hr></hr>
       <div>
-        <p>
-          Your height: &nbsp;
-          <input type="number" onChange={(e) => setHeight(e.target.value)} /> &nbsp;
-          <label><input type="radio" id="cm" name="height" value="cm" checked={true} onChange={() => setHeightUnit("cm")} />cm</label>
+
+        <form>
+          <label htmlFor="height">Height: </label>
+          <input min="1" type="number" id="height" /> &nbsp;
+          <label><input type="radio" id="cm" name="height" value="cm" onChange={() => setHeightUnit("cm")} />cm</label>
           <label><input type="radio" id="in" name="height" value="in" onChange={() => setHeightUnit("in")} />in</label>
-        </p>
-      </div>
-      <div>
-        <p>
-          Your weight: &nbsp;
-          <input type="number" onChange={(e) => setWeight(e.target.value)} /> &nbsp;
-          <label><input type="radio" id="kg" name="weight" value="kg" checked={true} onChange={() => setWeightUnit("kg")} />kg</label>
+          <br></br>
+          <br></br>
+          <label htmlFor="weight" style={{marginTop: "40px"}}>Weight: </label>
+          <input min="1" type="number" id="weight" /> &nbsp;
+          <label><input type="radio" id="kg" name="weight" value="kg"  onChange={() => setWeightUnit("kg")} />kg</label>
           <label><input type="radio" id="lbs" name="weight" value="lbs" onChange={() => setWeightUnit("lbs")} />lbs</label>
-        </p>
+          <br></br>
+          <br></br>
+          <button onClick={handleSubmit}>Submit</button>
+        </form>
       </div>
       <div>
+        <br></br>
         {
-          displayText()
+          isBmiValid ? bmiTextGenerator() : `Please enter your data.`
         }
       </div>
       <hr></hr>
       <div>
         <h3>BMI Categories</h3>
-        <p id="underweight">Underweight: under 18.5</p>
-        <p id="normalweight">Normal weight: 18.5 - 24.9</p>
-        <p id="overweight">Overweight: 25 - 29.9</p>
-        <p id="obese">Obese: 30 and over</p>
+        <p>Underweight: under 18.5</p>
+        <p>Normal weight: 18.5 - 24.9</p>
+        <p>Overweight: 25 - 29.9</p>
+        <p>Obese: 30 and over</p>
       </div>
     </div>
   );
-
-  function displayText() {
-    const heightInCm = heightUnit === "cm" ? height / 100 : (height * 2.54) / 100;
-    const weightInKg = weightUnit === "kg" ? weight : weight / 2.204623;
-    const bmi = Math.round((weightInKg / (heightInCm * heightInCm)) * 100) / 100;
-
-    return (
-      <div>
-        {bmi ? `Your BMI is ${bmi}.` : ``}
-        {bmi ? <br /> : ``}
-        {bmi < 18.5 ? `You are underweight.` : bmi <= 24.9 ? `You are of normal weight.` : bmi >= 25 && bmi < 30 ? `You are overweight.` : bmi >= 30 ? `You are obese.` : ``}
-      </div>
-    );
-  }
 }
 
 export default App;
